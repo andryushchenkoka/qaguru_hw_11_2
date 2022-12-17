1bcd9938891798e316016546fdf9687a2530c89a
KirillAndryushchenko <kirill2goo@gmail.com>
2022-12-17 23:42:02 +0300

    Hover test for task 1

--- src/test/java/tests/GithubTest.java
+++ src/test/java/tests/GithubTest.java
@@ -1,7 +1,34 @@
 package tests;
 
-public class GithubTest extends BaseTest{
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byTagAndText;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
 
+public class GithubTest extends BaseTest {
 
+    @Test
+    public void hoverTest() {
+        SelenideElement solutionsButton = $$("[aria-label = 'Global']").findBy(text("Solutions"));
+
+        step("Открыть главную страницу Github", () -> {
+            open("");
+        });
+
+        step("В верхнем меню навести курсор на Solutions", () -> {
+            solutionsButton.hover();
+        });
+
+        step("Кликнуть по разделу Enterprise из открывшегося списка", () -> {
+            solutionsButton.$(byText("Enterprise")).click();
+        });
+
+        step("На открытой странице должен быть заголовок с текстом Build like the best", () -> {
+            $(byTagAndText("h1", "Build like the best")).shouldBe(visible);
+        });
+    }
 }
