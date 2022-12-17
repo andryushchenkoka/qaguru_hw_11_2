a9590d79cbc6e0ae303334fe4c62743bcda89000
KirillAndryushchenko <kirill2goo@gmail.com>
2022-12-17 23:19:28 +0300

    Base test class

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,17 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
+
+public class BaseTest {
+
+    @BeforeAll
+    public static void beforeAll(){
+        Configuration.baseUrl = "https://github.com/";
+        Configuration.browserSize = "1920x1080";
+
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+}
